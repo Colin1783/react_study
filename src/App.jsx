759847444b94469ef40f9a5ed720489c9d25c319
Colin <hiedra97@gmail.com>
2024-05-09 20:03:16 +0900

    필요 없는 코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-// built-in component : html의 기본 요소, 소문자로 시작
-// component : 직접 만든 요소 (태그), 대문자로 시작
-
-// component는 return을 가진 함수
-function MyComponent() {
-  // return : 화면에 출력할 코드 (jsx code)
-  return <div>hello component</div>;
-}
-
-function MyComp() {
-  // jsx 코드: <div></div>
-  const myElem = <h1>hi JSX</h1>;
-
-  return myElem;
-}
-
 function App() {
-  // component 사용 시 종료 태그 생략할 수 없음
   return (
-    <>
-      <div>
-        <h1>hello react</h1>
-        <h2>하이 리액트</h2>
-        <h3>안녕 리액트</h3>
-        <p>Lorem ipsum dolor.</p>
-        <MyComponent />
-        <MyComp />
-        <hr /> {/* <hr> 은 불가능 */}
-      </div>
-    </>
+    <div>
+      <h1>hello react</h1>
+    </div>
   );
 }
 
