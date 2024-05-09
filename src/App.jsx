736115b84dcb6706a6581b70faf415f54faa3a7b
Colin <hiedra97@gmail.com>
2024-05-09 21:53:35 +0900

    javascrpit object

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 function App() {
-  // javascript review
+  //object : {} 안에 property 명: property 값(key:value)
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" };
 
-  //data type
-  let a; // undefined
-  console.log("a", typeof a);
-
-  let b = "문자열"; //string
-  console.log("b", typeof b);
-
-  let c = 3; // number
-  console.log("c", typeof c);
-
-  let d = 3.14;
-  console.log("d", typeof d);
-
-  let e = true; //boolean
-  console.log("e", typeof e);
-
-  let f = {}; // object
-  let g = []; // object
-  console.log("f", typeof f);
-  console.log("g", typeof g);
-
-  let h = function () {};
-  console.log("h", typeof h);
-  h = {};
-  console.log("h", typeof h);
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
 
   return <div></div>;
 }
