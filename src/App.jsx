99be644f8d09fca596620fa8bb37679eaced5a95
Colin <hiedra97@gmail.com>
2024-05-09 20:16:34 +0900

    컴포넌트 만들기 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+function MyHeader() {
+  return (
+    <div>
+      <h1> hello header</h1>
+      <h3> hello sub header</h3>
+    </div>
+  );
+}
+
+function MyFooter() {
+  return (
+    <div>
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    </div>
+  );
+}
+
+function MyNav() {
+  return (
+    <>
+      <a href="#">link1</a>
+      <a href="#">link2</a>
+    </>
+  );
+}
+
 function App() {
   return (
     <div>
