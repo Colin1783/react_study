a0b78fd997e1374c4c7ea112509c215a9e36233a
Colin <hiedra97@gmail.com>
2024-05-09 22:31:47 +0900

    배열 map 메소드로 배열의 각 원소를 컴포넌트로 매핑할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
 import React from "react";
 
 function App(props) {
-  //배열
-  const a = [];
+  const a = ["흥민", "강인", "희찬"];
+  const b = a.map(function (item) {
+    return item + "선수";
+  });
 
-  // push: 새 값을 배열 맨 뒤에 추가
-  a.push("흥민");
-  a.push("강인");
-  a.push(3.14);
-  console.log(a);
-
-  // pop: 맨 뒤의 값을 제거 후 반환
-  let val1 = a.pop();
-  console.log(val1);
-
-  let val2 = a.pop();
-  console.log(val2);
-  console.log(a);
-
-  // map: 각 원소를 다른 값으로 매핑(mapping)
-  const b = [5, 4, 3, 2, 1];
-  console.log(b);
-
-  function action(p) {
-    return p * 2;
-  }
-
-  const c = b.map(action);
-  console.log(c);
-
-  const d = b.map((item) => item * 3);
-  console.log(d);
-
-  return <div></div>;
+  return (
+    <>
+      <div>{a}</div>
+      <div>
+        {a[0]}
+        {a[1]}
+        {a[2]}
+      </div>
+      <div>{b}</div>
+      <div>
+        {b[0]}
+        {b[1]}
+        {b[2]}
+      </div>
+      <div>
+        {a.map((item) => (
+          <li>{item} 선수</li>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
