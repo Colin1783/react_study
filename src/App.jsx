a37d593020c6ffb5e61b9e1b60303ce312c8a901
Colin <hiedra97@gmail.com>
2024-05-09 22:07:01 +0900

    javascrpit object와 property 연습2 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 function App(props) {
-  const obj = {
-    age: 30,
-    name: "John Doe",
-    email: "john@example.com",
-    password: "1234567890",
+  const son = {
+    lastName: "손",
+    firstName: "흥민",
+    address: "런던",
+    country: "영국",
+    married: false,
   };
-
   return (
     <div>
-      name: {obj.name}
-      <br />
-      age: {obj.age}
-      <br />
-      email: {obj.email}
-      <br />
-      password: {obj.password}
+      <ul>
+        <li>성: {son.lastName}</li>
+        <li>이름: {son.firstName}</li>
+        <li>주소: {son.address}</li>
+        <li>국가: {son.country}</li>
+        <li>결혼 여부: {son.married ? "기혼" : "미혼"}</li>
+      </ul>
     </div>
   );
 }
