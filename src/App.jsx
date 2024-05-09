42cd5de736f21abe85bf81840e1723dff1dc1ed5
Colin <hiedra97@gmail.com>
2024-05-09 21:58:47 +0900

    javascrpit object와 property

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
   console.log("b", b);
   console.log("c", c);
 
+  console.log("c city", c.city);
+  console.log("c address", c.address);
+  console.log("c country", c.country);
+
+  b.city = "런던";
+  b.country = "영국";
+  console.log("b city", b.city);
+  console.log("b country", b.country);
+
   return <div></div>;
 }
 
