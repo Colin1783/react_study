3d939c51501a6031d07fc6856700897d99c44f87
Colin <hiedra97@gmail.com>
2024-05-09 21:43:15 +0900

    falsy, truthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+function App() {
+  // &&, ||
+  // falsy, truthy
+  // falsy : "", null, 0, undefined, false, NaN...
 
-function App(props) {
+  const a = 0;
+  if (a) {
+    console.log("a는 참");
+  } else {
+    console.log("a는 거짓");
+  }
+  // truthy : falsy 제외
+  // &&, || 와 조합
+  let b = 0; // or 0
+  let c = b && "값이 있음"; //
+
+  console.log("c", c);
+
+  let d = b || "값이 없음";
+  console.log("d", d);
+
   return <div></div>;
 }
 
