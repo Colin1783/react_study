77d2ccff92f0e92073b4ef35f54eff12752c078e
Colin <hiedra97@gmail.com>
2024-05-09 22:12:04 +0900

    필요 없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object
-
-  const a = {
-    name: "john",
-  };
-
-  const b = a;
-
-  console.log("a.name", a.name);
-  console.log("b.name", b.name);
-
-  b.name = "jane";
-  console.log("a.name", a.name);
-  console.log("b.name", b.name);
-
   return <div></div>;
 }
 
