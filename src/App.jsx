e28edd69ec2739bd1f1b8d90abf21fc060797e35
Colin <hiedra97@gmail.com>
2024-05-10 14:43:14 +0900

    props 값 없을 때 default

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+function MyComp({ name, age, team = "F/A" }) {
+  return (
+    <div>
+      <ul>
+        <li>name: {name}</li>
+        <li>age: {age}</li>
+        <li>team: {team}</li>
+      </ul>
+    </div>
+  );
+}
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name="John" age={25} team="A" />
+      <MyComp name="Doe" age={30} team="B" />
+      <MyComp name="Jane" age={35} />
+    </div>
+  );
 }
 
 export default App;
