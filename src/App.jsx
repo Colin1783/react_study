f90b90ed8f14b1a5017a3eb4c7f095776f9fa60e
Colin <hiedra97@gmail.com>
2024-05-10 15:03:26 +0900

    코드지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, team = "F/A" }) {
-  return (
-    <div>
-      <ul>
-        <li>name: {name}</li>
-        <li>age: {age}</li>
-        <li>team: {team}</li>
-      </ul>
-    </div>
-  );
-}
 function App(props) {
-  return (
-    <div>
-      <MyComp name="John" age={25} team="A" />
-      <MyComp name="Doe" age={30} team="B" />
-      <MyComp name="Jane" age={35} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
