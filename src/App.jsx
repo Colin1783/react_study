6cf135d0b920a2bcc3d25abe58ad9d1ecd8b5023
Colin <hiedra97@gmail.com>
2024-05-10 09:48:27 +0900

    onClick property와 함수 사용1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // 이름 있는 함수
+  function action1() {
+    console.log("action1 called");
+  }
+  // 이름 없는 함수
+  const action2 = function () {
+    console.log("anonymous function called");
+  };
+
+  action1();
+
+  return (
+    <div>
+      <button onClick={action1}>클릭1</button>
+      <button onClick={action2}>클릭2</button>
+      <button
+        onClick={function () {
+          console.log("anonymous function2 called");
+        }}
+      >
+        클릭3
+      </button>
+    </div>
+  );
 }
 
 export default App;
