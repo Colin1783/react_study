2fdd1a159ba2bb990693ed3897f7b21d911ccfe0
Colin <hiedra97@gmail.com>
2024-05-10 14:14:52 +0900

    코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
 import React from "react";
 
-// props : properties
-// <MyButton name="click3" />
-// props.name = "click3"
-
-function MyButton(props) {
-  return (
-    <button
-      style={{
-        padding: "10px",
-        backgroundColor: "red",
-        color: "white",
-      }}
-    >
-      {" "}
-      {props.name}{" "}
-    </button>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <button>click1</button>
-      <br />
-      <button>click2</button>
-      <br />
-      <MyButton name="click3" />
-      <br />
-      <MyButton name="click4" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
