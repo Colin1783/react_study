cc5cca20faa6640d7c02749b79e5fecbcc13f842
Colin <hiedra97@gmail.com>
2024-05-10 15:12:07 +0900

    코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
 
-function MyComponent(props) {
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComponent
-        // prop의 값 타입은 javascript 값 타입
-        // {}에 값 입력(String은 생략 가능)
-        font={"bold"}
-        name={"son"}
-        street="seoul"
-        age={3.14}
-        address={{ city: "seoul", country: "us" }}
-        married={true}
-        action={function () {
-          console.log("hello");
-        }}
-        someAction={() => console.log("someAction")}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
