172ba465a18a6d64c732d8b271b9db00b6b8eda1
Colin <hiedra97@gmail.com>
2024-05-10 16:09:10 +0900

    리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-// style props
-// style={{}} : style props 값은 객체여야 합니다.
 
 function App(props) {
-  return (
-    <div>
-      <div
-        style={{
-          color: "blue",
-          padding: "20px",
-          border: "1px dashed black",
-          backgroundColor: "yellow",
-          textAlign: "center",
-          fontSize: "2rem",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
