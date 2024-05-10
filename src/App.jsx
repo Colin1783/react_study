b8df0f37f5a0add4419aefc2aefba1b287cca1ba
Colin <hiedra97@gmail.com>
2024-05-10 14:13:44 +0900

    props (properties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+// props : properties
+// <MyButton name="click3" />
+// props.name = "click3"
+
+function MyButton(props) {
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "red",
+        color: "white",
+      }}
+    >
+      {" "}
+      {props.name}{" "}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button>click1</button>
+      <br />
+      <button>click2</button>
+      <br />
+      <MyButton name="click3" />
+      <br />
+      <MyButton name="click4" />
+    </div>
+  );
 }
 
 export default App;
