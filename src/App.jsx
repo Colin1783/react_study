035a6f18daa1c067ecfd69d7bae898f1fb20f306
Colin <hiedra97@gmail.com>
2024-05-10 16:08:44 +0900

    style props 은 객체로 css property 명은 lowerCamelCase로

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+// style props
+// style={{}} : style props 값은 객체여야 합니다.
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div
+        style={{
+          color: "blue",
+          padding: "20px",
+          border: "1px dashed black",
+          backgroundColor: "yellow",
+          textAlign: "center",
+          fontSize: "2rem",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
 }
 
 export default App;
