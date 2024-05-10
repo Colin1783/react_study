5d567aa42d39690d135957a177e1bef55ab94497
Colin <hiedra97@gmail.com>
2024-05-10 16:18:39 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
-function MyComponent({ headColor, bodyColor }) {
-  return (
-    <div>
-      <h1 style={{ color: headColor }}>Lorem.</h1>
-      <p style={{ color: bodyColor }}>Lorem ipsum dolor.</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComponent headColor="red" bodyColor="blue" />
-      <MyComponent headColor="green" bodyColor="purple" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
