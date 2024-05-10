3431838de45c125ebf7dde454648503171940322
Colin <hiedra97@gmail.com>
2024-05-10 15:38:47 +0900

    코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log("name", name);
-  console.log("age", age);
-  console.log("someProp", someProp); // default = true
-  console.log("otherProp", otherProp); // 값 부여 가능(true or false)
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="John" age={30} someProp otherProp={false} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
