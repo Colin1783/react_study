b43a71ea5a20d5236b3060e644dbbf4798daa8b3
Colin <hiedra97@gmail.com>
2024-05-10 14:37:58 +0900

    props를 분해 할당으로 받기 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+function MyInfo({ name, age, email, country }) {
+  return (
+    <div>
+      <ul>
+        <li>name: {name}</li>
+        <li>age: {age}</li>
+        <li>email: {email}</li>
+        <li>country: {country}</li>
+      </ul>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyInfo name="John" age={30} email="1234@abcd.com" country="usa" />
+    </div>
+  );
 }
 
 export default App;
