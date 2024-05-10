201c1235a058323e75f4fd8c1f7775d85aea15c2
Colin <hiedra97@gmail.com>
2024-05-10 09:24:18 +0900

    필요 없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = ["흥민", "강인", "희찬"];
-  const b = a.map(function (item) {
-    return item + "선수";
-  });
-
-  return (
-    <>
-      <div>{a}</div>
-      <div>
-        {a[0]}
-        {a[1]}
-        {a[2]}
-      </div>
-      <div>{b}</div>
-      <div>
-        {b[0]}
-        {b[1]}
-        {b[2]}
-      </div>
-      <div>
-        {a.map((item) => (
-          <li>{item} 선수</li>
-        ))}
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
