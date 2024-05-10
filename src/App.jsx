f8bae7da2883e4e090c3138a34844818930988fe
Colin <hiedra97@gmail.com>
2024-05-10 14:02:37 +0900

    코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object ({},[])
-  const a = [6, 7, 8];
-  const [b, c, d] = a;
-  console.log(b); // 6
-  console.log(c); // 7
-  console.log(d); // 8
-
-  // 나머지 모두
-  const e = [6, 7, 8, 9, 10];
-  const [f, g, ...h] = e;
-  console.log(f); // 6
-  console.log(g); // 7
-  console.log(h); // [8, 9, 10]
-  console.log(h[0]); // 8
-  console.log(h[1]); // 9
-  console.log(h[2]); // 10
-
-  const i = ["pizza", "pasta", "bread"];
-  const j = i;
-  const [...k] = i;
-
-  k[2] = "salad";
-  console.log(j); // ["pizza", "pasta", "bread"]
-  console.log(k); // ["pizza", "pasta", "salad"]
-
   return <div></div>;
 }
 
