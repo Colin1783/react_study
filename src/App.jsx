cf1c1185972ba9b34565fb5cc2f3b1e7d877c5c8
Colin <hiedra97@gmail.com>
2024-05-10 15:20:34 +0900

    코드 지우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
 import React from "react";
-// props 이름은 lowerCamelCase로 작성합니다.
-// 예약어는 사용하지 않습니다.(예: class -> className, for -> htmlFor, let -> letValue...)
-
-function MyComp(props) {
-  return <div></div>;
-}
 
 function App(props) {
-  return (
-    <div>
-      <label htmlFor="input1">name</label>
-      <input id={"input1"} type="text" />
-      <p className={"error note"}></p>
-      <MyComp number={3} pageNumber={3} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
