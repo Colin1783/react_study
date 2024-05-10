1c76dddfd8fbbbe19eaa41a456ee52e15361ed6b
Colin <hiedra97@gmail.com>
2024-05-10 16:18:27 +0900

    style prop 응용 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function MyComponent({ headColor, bodyColor }) {
+  return (
+    <div>
+      <h1 style={{ color: headColor }}>Lorem.</h1>
+      <p style={{ color: bodyColor }}>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComponent headColor="red" bodyColor="blue" />
+      <MyComponent headColor="green" bodyColor="purple" />
+    </div>
+  );
 }
 
 export default App;
