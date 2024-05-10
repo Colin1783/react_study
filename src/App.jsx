6faef7a633ab3248aec398081b53dcf4ab3dac3f
Colin <hiedra97@gmail.com>
2024-05-10 14:21:49 +0900

    props 넘기는 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+function MyComp(props) {
+  return (
+    <div>
+      props.name: {props.name}
+      <br />
+      props.age: {props.age}
+      <br />
+      props.address: {props.address}
+      <br />
+      props.city: {props.city}
+      <br />
+      props.country: {props.country}
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div id={""}>
+      <MyComp name="son" age={33} address="seoul" />
+      <MyComp city="busan" country="korea" />
+    </div>
+  );
 }
 
 export default App;
