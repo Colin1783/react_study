270d09f7494a33dbb9e21730027907d20da70447
Colin <hiedra97@gmail.com>
2024-05-10 15:23:21 +0900

    props에 값이 없이 이름만 작성되면 true(default)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+function MyComp({ name, age, someProp, otherProp }) {
+  console.log("name", name);
+  console.log("age", age);
+  console.log("someProp", someProp); // default = true
+  console.log("otherProp", otherProp); // 값 부여 가능(true or false)
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name="John" age={30} someProp otherProp={false} />
+    </div>
+  );
 }
 
 export default App;
