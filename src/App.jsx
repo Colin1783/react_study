d913da9654df174181273f69b676b073f576ef47
Colin <hiedra97@gmail.com>
2024-05-10 16:21:57 +0900

    style props 응용 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+function MyComponent({ headStyle, bodyStyle }) {
+  return (
+    <div>
+      <h1 style={headStyle}>Hello, World!</h1>
+      <p style={bodyStyle}>This is a simple component.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComponent
+        headStyle={{
+          padding: "5px",
+          border: "1px solid black",
+          padding: "10px",
+        }}
+      />
+      <MyComponent
+        bodyStyle={{
+          margin: "5px",
+          border: "1px dotted skyblue",
+          color: "red",
+          textAlign: "center",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
