e594d36f1612189fb766d4307e020f5d4c63c1d6
Colin <hiedra97@gmail.com>
2024-05-13 21:47:37 +0900

    코드초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import { v1, v2 } from "./Value1.js";
-import { c1, c2 } from "./Value2.js";
 
 function App(props) {
-  return (
-    <div>
-      <div>{v1}</div>
-      <div>{v2}</div>
-      <div>{c1}</div>
-      <div>{c2}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,3 +1,19 @@
 export const myStr = "korea";
 export const myAddress = "seoul";
 export const value3 = 44;
+const value4 = {};
+const value5 = [];
+function action1() {
+  console.log("action1");
+  return null;
+}
+
+function action2() {
+  console.log("action2");
+  return null;
+}
+function action3() {
+  console.log("action3");
+  return null;
+}
+export { value4, value5, action1, action2, action3 };
