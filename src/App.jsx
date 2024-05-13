8d2169dfd912850736d9c2772c4a74f95df10578
Colin <hiedra97@gmail.com>
2024-05-13 22:53:52 +0900

    react는 re-rendering 시점을 적절한 시점으로 결정함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp1() {
+  const [val, setVal] = useState(0);
+
+  function updateVal() {
+    // react는 re-rendering 시점을 적절한 시점으로 결정함
+    setVal(val + 1);
+    setVal(val + 1);
+    setVal(val + 1);
+    console.log("val", val);
+  }
+  return (
+    <div>
+      <div>{val}</div>
+      <div>
+        <button onClick={updateVal}>증가</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp1 />
+    </div>
+  );
 }
 
 export default App;
