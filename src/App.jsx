06acc6eeffe444b2052cc5ad4b3a7df7682bcf1e
Colin <hiedra97@gmail.com>
2024-05-13 22:48:15 +0900

    상태가 객체이면 새 객체로 복사해서 써야함 react는 참조값이 같으면 같은 값으로 판단하기 때문

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp1() {
+  const [val, setVal] = useState([]);
+
+  function addItem() {
+    const [...newVal] = val; // [] -> ["a"]
+    newVal.push("a");
+    setVal(newVal
+  }
+
+  return (
+    <div>
+      <div>{val}</div>
+      <div>
+        <button onClick={addItem}>추가</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp1 />
+    </div>
+  );
 }
 
 export default App;
