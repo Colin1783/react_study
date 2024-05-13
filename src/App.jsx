4bb566baac9cb48a377274b4a09fb3a0d62b8014
Colin <hiedra97@gmail.com>
2024-05-13 22:26:17 +0900

    react 컴포넌트 상태 컴포넌트는 상태를 가지고 있음 상태가 변경되면 다시 그려짐 상태는 react가 관리함

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import val, { v1 } from "./myValues.jsx";
-import someVal from "./MyComps.jsx";
+import React, { useState } from "react";
 
-// default export는 아무 이름으로 import 가능하다.
-// default export는 한 파일에 한 개만 존재할 수 있다.
+function MyComp() {
+  // react conponent 는 상태(state)가 변경되면 다시 렌더링된다.
+  // 각 component의 state는 react가 관리한다.
+
+  let val = 0;
+
+  function increment() {
+    val++;
+    console.log(val);
+  }
+  return (
+    <div>
+      {val}
+      <button onClick={increment}> 증가 </button>
+    </div>
+  );
+}
+
+function MyComp2() {
+  // useState() 함수를 사용하여 상태를 관리할 수 있다.
+  // useState() 함수는 배열을 반환한다.
+  // 배열의 첫번째 요소는 상태값이고, 두번째 요소는 상태를 변경하는 함수이다.
+  // useState() 함수의 인자로 초기값을 전달할 수 있다.
+
+  const [val, setVal] = useState(0);
+
+  function increment() {
+    setVal(val + 1);
+  }
+
+  return (
+    <div>
+      {val}
+      <button onClick={increment}> 증가 </button>
+    </div>
+  );
+}
 
 function App(props) {
   return (
     <div>
-      <h1>App</h1>
-      <ul>
-        <li>v1: {v1}</li>
-        <li>v2: {val}</li>
-        <li>v3: {someVal}</li>
-      </ul>
+      <MyComp />
+      <MyComp2 />
     </div>
   );
 }
