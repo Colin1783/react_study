8f3be4c1c64284d35a56ebfec7a6a5dcd941ed63
Colin <hiedra97@gmail.com>
2024-05-13 21:35:18 +0900

    as로 별칭 주기 가능, 여러 파일에 같은변수가 있으면 as로 충돌 피할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+// import: 다른 파일에 있는 변수를 불러올 때 사용
+import { value1, value2, value3 as v3, value4 } from "./MyValues.js"; // 여러개 불러오기
+import { myAddress, myStr, value3 } from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      value1: {value1}
+      <br />
+      value2: {value2}
+      <br />
+      value3: {v3}
+      <br />
+      value4.name: {value4.name}
+      <br />
+      value4.team: {value4.team}
+      <br />
+      <div>{myStr}</div>
+      <div>{myAddress}</div>
+      <div>{value3}</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyValues.js
@@ -0,0 +1,8 @@
+// export : 다른 파일에서 사용할 수 있도록 내보내는 기능
+
+const value1 = "son";
+const value2 = "lee";
+const value3 = 44;
+const value4 = { name: "kim", team: "뮌헨" };
+
+export { value1, value2, value3, value4 }; // 여러개 내보내기
