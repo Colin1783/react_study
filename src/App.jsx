d101c35dfd3a1e0afee6d18046c243f229af912d
Colin <hiedra97@gmail.com>
2024-05-13 21:11:28 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <Button colorScheme="blue">Button</Button>
-        <Button colorScheme="orange">Button</Button>
-        <Button
-          sx={{
-            color: "skyblue",
-            fontSize: "40px",
-            padding: "20px",
-          }}
-          colorScheme={"gray"}
-        >
-          클릭
-        </Button>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
