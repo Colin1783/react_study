95e7a2e00f3358068e16205b6ed7899842450144
Colin <hiedra97@gmail.com>
2024-05-13 21:50:24 +0900

    react component 생성 방법들
    이름 있는 함수
    이름 없는 함수
    화살표 함수

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,19 +1,11 @@
-export const myStr = "korea";
-export const myAddress = "seoul";
-export const value3 = 44;
-const value4 = {};
-const value5 = [];
-function action1() {
-  console.log("action1");
-  return null;
+export function MyComps() {
+  return <div>myComp</div>;
 }
 
-function action2() {
-  console.log("action2");
-  return null;
-}
-function action3() {
-  console.log("action3");
-  return null;
-}
-export { value4, value5, action1, action2, action3 };
+export const YourComp = function () {
+  return <div>your comp</div>;
+};
+
+export const ArrowComp = () => {
+  return <div>Arrow comp</div>;
+};
