c64d7c96bac764305f1511946001713406df16cd
Colin <hiedra97@gmail.com>
2024-05-13 21:52:59 +0900

    여러 변수 export를 한번에 할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { v1, v2, v3 } from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <h1>App</h1>
+      <ul>
+        <li>v1: {v1}</li>
+        <li>v2: {v2}</li>
+        <li>v3: {v3}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyComps.jsx
@@ -0,0 +1,5 @@
+const v1 = "korea";
+const v2 = 44;
+export const v3 = 3.14;
+
+export { v1, v2 };
