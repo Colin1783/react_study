810913764e594fc735d99ee438c19d2988051665
Colin <hiedra97@gmail.com>
2024-05-13 23:28:22 +0900

    state3 연습 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = React.useState("");
+
+  const isTooLong = text.length > 10;
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
+      {isTooLong && <p>Text is too long!</p>}
+    </div>
+  );
 }
 
 export default App;
