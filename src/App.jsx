9df5f337aee09feccf41805efd6abdc6abee7c89
Colin <hiedra97@gmail.com>
2024-05-14 12:25:16 +0900

    컨텍스트로 데이터(객체) 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+
+// eslint-disable-next-line no-unused-vars
+const TextContext = createContext("");
+
+function MyInput() {
+  const textHandler = useContext(TextContext);
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => textHandler.updateText(e.target.value)}
+      />
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
+
+function MyText() {
+  const textHandler = useContext(TextContext);
+
+  return (
+    <div>
+      <p>{textHandler.text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(t) {
+    setText(t);
+  }
+
+  return (
+    <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
+      <div>
+        <MyInput />
+        <hr />
+        <MyText />
+      </div>
+    </TextContext.Provider>
+  );
 }
 
 export default App;
