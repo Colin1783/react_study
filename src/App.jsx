9f32bf60823f936dfdfaf9ceaa80ece04f8cec30
Colin <hiedra97@gmail.com>
2024-05-14 09:20:49 +0900

    컴포넌트는 각각의 상태를 가짐

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+function MyComp() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+      </div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
