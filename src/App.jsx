49c4a94f8f07932037a3f5cc587b051b87a9e668
Colin <hiedra97@gmail.com>
2024-05-14 15:09:02 +0900

    useEffect 의 첫번쨰 parameter에서 dependency를 변경하지 않도록 주의할 것

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,20 @@
 import React, { useEffect, useState } from "react";
 
-function MyComp() {
-  const [count, setCount] = useState(0);
-  const [text1, setText1] = useState("");
-  const [text2, setText2] = useState("");
-
-  // 특정 순간에 실행되는 메소드를 파라미터로 받음
-  // 특정 순간 : 예) initial render, unmount, update, re-render..
-  // 첫번째 파라미터 : 함수
-  // 두번째 파라미터 : 배열. dependency array
-  // 두번째 파라미터가 없으면 initial render 시에만 실행
-  // 두번째 파라미터가 있으면, 배열에 있는 값이 바뀌었을 때만 실행
-  //
-  useEffect(() => {
-    console.log("every render");
-  });
-
-  // 첫번째 파라미터(함수)의 리턴은 unmount 시에 실행됨+++++
-  useEffect(() => {
-    console.log("unmount 될 때 실행되는 메소드");
-  });
-
-  //어떤 값(주로 state, dependency)이 바뀌었을 때만 실행
-  useEffect(() => {
-    console.log("text1 값이 바뀌었을 때 실행");
-  }, [text1]);
-
-  useEffect(() => {
-    console.log("text2 값이 바뀌었을 때 실행");
-  }, [text2]);
-
-  useEffect(() => {
-    console.log("text1 또는 text2 값이 바뀌엇을 때");
-  }, [text1, text2]);
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <div>
-        <h1>{text1}</h1>
-        <input type="text" onChange={(e) => setText1(e.target.value)} />
-      </div>
-      <div>
-        <h1>{text2}</h1>
-        <input type="text" onChange={(e) => setText2(e.target.value)} />
-      </div>
-    </div>
-  );
-}
-
 function App(props) {
-  const [show, setShow] = useState(true);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Component mounted"); // 새로고침할 때(mount)마다 실행
+  }, []);
+
+  useEffect(() => {
+    console.log("count updated"); // count가 업데이트 될 때마다 실행
+    //useEffect 내에서 dependency를 조작하면 무한루프에 빠질 수 있음
+    //setCount(count + 1); // 무한루프
+  }, [count]);
 
   return (
     <div>
-      <input
-        type="checkbox"
-        checked={show}
-        onChange={(e) => setShow(e.target.checked)}
-      />
-      {show && <MyComp />}
+      <button onClick={(e) => setCount(count + 1)}>click</button>
     </div>
   );
 }
