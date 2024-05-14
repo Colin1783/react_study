dd2b5a80fd43b6652e1710d58735c44eec3793d7
Colin <hiedra97@gmail.com>
2024-05-14 12:13:18 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
-
-// This is a simple example of passing data deeply with props.
-//context
-// https://react.dev/learn/passing-data-deeply-with-context
-
-const MessageContext = createContext("");
-
-function MyBox() {
-  const message = useContext(MessageContext);
-  return <div>{message}</div>;
-}
-
-function MySection() {
-  return <MyBox />;
-}
-
-function MyContainer() {
-  return <MySection />;
-}
+import React from "react";
 
 function App(props) {
-  const [message, setMessage] = useState("");
-  return (
-    <MessageContext.Provider value={message}>
-      <div>
-        <input type="text" onChange={(e) => setMessage(e.target.value)} />
-        <p>{message}</p>
-        <hr />
-        <MyContainer />
-      </div>
-    </MessageContext.Provider>
-  );
+  return <div></div>;
 }
 
 export default App;
