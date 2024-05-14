cbbbcc9b21c2c9bf8177e9600613709c4bac1c1d
Colin <hiedra97@gmail.com>
2024-05-14 10:01:59 +0900

    컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App(props) {
       <input
         type="checkbox"
         checked={view1}
-        onClick={(e) => setView1(e.target.checked)}
+        onChange={(e) => setView1(e.target.checked)}
       />
       {view1 && <MyBox color={"blue"} />}
       <hr />
@@ -29,7 +29,7 @@ function App(props) {
         <input
           type="checkbox"
           checked={view1}
-          onClick={(e) => setView2(e.target.checked)}
+          onChange={(e) => setView2(e.target.checked)}
         />
         {view2 && <MyBox color={"red"} />}
       </div>
