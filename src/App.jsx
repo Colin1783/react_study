a355f10b192ba7e34a91dc5b40c377b6c7bcd35a
Colin <hiedra97@gmail.com>
2024-05-14 14:10:58 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
-
-// eslint-disable-next-line no-unused-vars
-const TextContext = createContext("");
-
-function MyInput() {
-  const textHandler = useContext(TextContext);
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => textHandler.updateText(e.target.value)}
-      />
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
-
-function MyText() {
-  const textHandler = useContext(TextContext);
-
-  return (
-    <div>
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(t) {
-    setText(t);
-  }
-
-  return (
-    <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
-      <div>
-        <MyInput />
-        <hr />
-        <MyText />
-      </div>
-    </TextContext.Provider>
-  );
+  return <div></div>;
 }
 
 export default App;
