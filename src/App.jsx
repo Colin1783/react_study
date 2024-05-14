d423480ab41a021d3bb6b26d7ce6ec66b5595ad0
Colin <hiedra97@gmail.com>
2024-05-14 10:18:12 +0900

    컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ color }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      style={{ border: `2px solid ${color}`, padding: "10px", margin: "10px" }}
+    >
+      <button onClick={(e) => setCount(count + 1)}>up</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [checked, setChecked] = useState(true);
+  return (
+    <div>
+      <input type="checkbox" onChange={(e) => setChecked(e.target.checked)} />
+      {checked && <MyBox color={"red"} />}
+    </div>
+  );
 }
 
 export default App;
