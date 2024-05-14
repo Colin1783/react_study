c25487f1f976e6c1ae910483e6adc19ba349ac39
Colin <hiedra97@gmail.com>
2024-05-14 09:46:16 +0900

    컴포넌트가 트리에서 떨어진 후 다시 붙으면 initial rendering 됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ color }) {
+  const [num, setNum] = useState(0);
+
+  return (
+    <div>
+      <div style={{ border: `5px solid ${color}` }}>
+        <button onClick={(e) => setNum(num + 1)}>증가</button>
+        <p>{num}</p>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [view1, setView1] = useState(true);
+  const [view2, setView2] = useState(true);
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={view1}
+        onClick={(e) => setView1(e.target.checked)}
+      />
+      {view1 && <MyBox color={"blue"} />}
+      <hr />
+      <div>
+        <input
+          type="checkbox"
+          checked={view1}
+          onClick={(e) => setView2(e.target.checked)}
+        />
+        {view2 && <MyBox color={"red"} />}
+      </div>
+    </div>
+  );
 }
 
 export default App;
