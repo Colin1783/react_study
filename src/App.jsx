52e2d67579cea77923b2f3bc19f1a6d862dc0090
Colin <hiedra97@gmail.com>
2024-05-14 16:09:49 +0900

    axios 로 ajax get 요청 연습 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
   function handleClick1() {
@@ -6,8 +7,8 @@ function App(props) {
   }
 
   function handleClick2() {
-    axios.get("/path1", {
-      param: {
+    axios.get("/path2?", {
+      params: {
         city: "Seoul",
         address: "Gangnam",
       },
@@ -18,7 +19,7 @@ function App(props) {
     const params = new URLSearchParams();
     params.append("name", "Alice");
     params.append("age", 20);
-    axios.get("/path1" + params.toString());
+    axios.get("/path3?" + params.toString());
   }
 
   return (
@@ -27,10 +28,10 @@ function App(props) {
         <button onClick={handleClick1}>/path1 get 요청</button>
       </div>
       <div>
-        <button onClick={handleClick2}>/path1 get 요청 city, address</button>
+        <button onClick={handleClick2}>/path2 get 요청 city, address</button>
       </div>
       <div>
-        <button onClick={handleClick3}>/path1 get 요청 name, age</button>
+        <button onClick={handleClick3}>/path3 get 요청 name, age</button>
       </div>
     </div>
   );
