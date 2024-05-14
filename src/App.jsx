ad01df6f9b62c2b2cf0eebb0fa55a9b7de9f9160
Colin <hiedra97@gmail.com>
2024-05-14 10:18:44 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,7 @@
-import React, { useState } from "react";
-
-function MyBox({ color }) {
-  const [num, setNum] = useState(0);
-
-  return (
-    <div>
-      <div style={{ border: `5px solid ${color}` }}>
-        <button onClick={(e) => setNum(num + 1)}>증가</button>
-        <p>{num}</p>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [view1, setView1] = useState(true);
-  const [view2, setView2] = useState(true);
-  return (
-    <div>
-      <input
-        type="checkbox"
-        checked={view1}
-        onChange={(e) => setView1(e.target.checked)}
-      />
-      {view1 && <MyBox color={"blue"} />}
-      <hr />
-      <div>
-        <input
-          type="checkbox"
-          checked={view1}
-          onChange={(e) => setView2(e.target.checked)}
-        />
-        {view2 && <MyBox color={"red"} />}
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
