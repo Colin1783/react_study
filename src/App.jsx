5aafb22e842fc5200c22f3279c99cc7e0f162ab7
Colin <hiedra97@gmail.com>
2024-05-14 09:25:18 +0900

    컴포넌트는 각각의 상태를 가지는 거 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+    </div>
+  );
 }
 
 export default App;
