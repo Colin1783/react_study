579745508878bf75fa72c23e59cb182e565a903c
Colin <hiedra97@gmail.com>
2024-05-16 12:17:45 +0900

    json 타입이 해당 타입으로 파싱되는지 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  // json 데이터 타입 : object, array, string, number, boolean, null
+
+  function handleClick1() {
+    const data = {
+      name: "홍길동",
+      age: 20,
+      married: false,
+      address: {
+        address1: "서울시",
+        address2: "강남구",
+        address3: "역삼동",
+      },
+      item: ["item1", "item2", "item3"],
+      money: null,
+    };
+
+    axios.post("/api/main41/sub5", data);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>json 데이터와 요청</button>
+    </div>
+  );
 }
 
 export default App;
