cef41e083a53d44e87bb4c6ecf884c6cd1966f38
Colin <hiedra97@gmail.com>
2024-05-16 16:05:06 +0900

    200 외의 응답은 catch 에 들어간 함수 실행

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,30 @@ function App(props) {
       .catch(() => console.log("catch의 메소드, 400응답"));
   }
 
+  function handleClick404() {
+    axios
+      .get("api/main43/sub3")
+      .then((response) => console.log("404응답"))
+      .catch(() => console.log("catch의 메소드, 404응답"));
+  }
+
+  function handleClick500() {
+    axios
+      .get("api/main43/sub4")
+      .then((response) => console.log("500응답"))
+      .catch(() => console.log("catch의 메소드, 500응답"));
+  }
+
   return (
     <div>
       <button onClick={handleClick200}> 200 응답</button>
+      <br />
       <button onClick={handleClick400}> 400 응답</button>
+      <br />
+      <button onClick={handleClick404}> 404 응답</button>
+      <br />
+      <button onClick={handleClick500}> 500 응답</button>
+      <br />
     </div>
   );
 }
