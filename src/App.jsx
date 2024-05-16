f3d0b6e03e7dac517ade8cee92dc50b691f6923a
Colin <hiedra97@gmail.com>
2024-05-16 11:41:39 +0900

    input 입력 값을 json으로 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState();
+
   function handleClick1() {
     const data = {
       name: "홍길동",
@@ -14,9 +17,32 @@ function App(props) {
     axios.post("/api/main41/sub1", data);
   }
 
+  function handleClick2() {
+    const data = {
+      food: ["짜장면", "짬뽕", "탕수육"],
+      price: [5000, 6000, 12000],
+      calories: [600, 800, 1500],
+    };
+
+    axios.post("/api/main41/sub2", data);
+  }
+
+  function handleClick3() {
+    axios.post("/api/main41/sub3", { name, city });
+  }
+
   return (
     <div>
       <button onClick={handleClick1}>요청(JSON 데이터 포함)</button>
+      <hr />
+      <button onClick={handleClick2}>요청(JSON 데이터 포함)</button>
+      <div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <br />
+        <input type="text" onChange={(e) => setCity(e.target.value)} />
+        <br />
+        <button onClick={handleClick3}>클릭</button>
+      </div>
     </div>
   );
 }
