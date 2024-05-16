e3d288aa3a2c95f2b3bac6001813cd53ed28cf5c
Colin <hiedra97@gmail.com>
2024-05-16 15:21:24 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  const [result, setResult] = React.useState("");
-
-  function handleClick1() {
-    axios.get("/api/main42/sub1").then((response) => {
-      console.log(response.data);
-    });
-  }
-
-  function handleClick2() {
-    axios.get("/api/main42/sub2").then((response) => {
-      setResult(response.data);
-    });
-  }
-
-  function handleClick3() {
-    axios
-      .get("/api/main42/sub3")
-      .then((response) => console.log(response.data));
-  }
-
-  function handleClick4() {
-    axios.get("/api/main42/sub4").then((response) => {
-      console.log(response.data);
-    });
-  }
-
-  function handleClick5() {
-    axios.get("/api/main42/sub5").then((response) => {
-      console.log(response.data);
-    });
-  }
-
-  function handleClick6() {
-    axios.get("/api/main42/sub6").then((response) => {
-      console.log(response.data);
-    });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>클릭</button>
-      <br />
-      <button onClick={handleClick2}> 클릭</button>
-      <br />
-      <p>{result}</p>
-      <br />
-      <button onClick={handleClick3}>크을릭</button>
-      <br />
-      <button onClick={handleClick4}>클릭</button>
-      <br />
-      <button onClick={handleClick5}>클릭5</button>
-      <br />
-      <button onClick={handleClick6}>클릭6</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
