01544751a5331d47adf808ad022f7e7395ffc78d
Colin <hiedra97@gmail.com>
2024-05-16 10:04:33 +0900

    get, post 요청 핸들러 메소드 작성2

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,21 @@ function App(props) {
     axios.post("/api/someurl", { data: "some data" });
   }
 
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton4() {
+    axios.post("/api/someurl2", { data: "some data" });
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
       <button onClick={handleClickButton2}>post 요청</button>
+      <hr />
+      <button onClick={handleClickButton3}>get 요청2</button>
+      <button onClick={handleClickButton4}>post 요청2</button>
     </div>
   );
 }
