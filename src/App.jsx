c783585ce594f4ba4cec531494c2415b23292c3a
Colin <hiedra97@gmail.com>
2024-05-16 14:29:26 +0900

    JSON 응답 파싱하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [result, setResult] = React.useState("");
+
+  function handleClick1() {
+    axios.get("/api/main42/sub1").then((response) => {
+      console.log(response.data);
+    });
+  }
+
+  function handleClick2() {
+    axios.get("/api/main42/sub2").then((response) => {
+      setResult(response.data);
+    });
+  }
+
+  function handleClick3() {
+    axios
+      .get("/api/main42/sub3")
+      .then((response) => console.log(response.data));
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>클릭</button>
+      <br />
+      <button onClick={handleClick2}> 클릭 </button>
+      <br />
+      <p>{result}</p>
+      <br />
+      <button onClick={handleClick3}>크을릭</button>
+      <br />
+    </div>
+  );
 }
 
 export default App;
