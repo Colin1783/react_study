1f5cd09bfb8ba12d23d326dcde028e83a36039a0
Colin <hiedra97@gmail.com>
2024-05-20 10:19:57 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 function App(props) {
   return (
     <ChakraProvider>
-      <Box bgColor={"tomato"} w={"500px"}>
-        Lorem ipsum dolor.
-      </Box>
-      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
-        Lorem ipsum dolor.
-      </Box>
+      <Box>Lorem ipsum dolor.</Box>
     </ChakraProvider>
   );
 }
