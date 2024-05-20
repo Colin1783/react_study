4d8343801cf73c2b26b206e81ee4b3c512eefd94
Colin <hiedra97@gmail.com>
2024-05-20 11:10:12 +0900

    form components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Checkbox,
+  Radio,
+  RadioGroup,
+  Switch,
+  Textarea,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Checkbox>Lorem ipsum.</Checkbox>
+      <Checkbox>Necessitatibus, totam.</Checkbox>
+      <Checkbox>Consequuntur, doloremque!</Checkbox>
+      <Checkbox>Inventore, quas.</Checkbox>
+      <hr />
+      <RadioGroup>
+        <Radio value={1}>1</Radio>
+        <Radio value={2}>2</Radio>
+        <Radio value={3}>3</Radio>
+      </RadioGroup>
+      <hr />
+      <Switch />
+      <hr />
+      <Textarea>Lorem ipsum dolor sit amet, consectetur.</Textarea>
+    </ChakraProvider>
+  );
 }
 
 export default App;
