a72dcef920191f0b2cbe0668df49317e43c36de4
Colin <hiedra97@gmail.com>
2024-05-20 11:03:47 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
 import React from "react";
-import {
-  Box,
-  Center,
-  ChakraProvider,
-  FormControl,
-  FormLabel,
-  Input,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <label htmlFor="input1">이름</label>
-      <input id="input1" type="text" />
-      <hr />
-      <Center>
-        <Box w={{ base: "100%", md: "50%" }}>
-          <FormControl>
-            <FormLabel htmlFor="input2">이름</FormLabel>
-            <Input id="input2" type="text" />
-          </FormControl>
-        </Box>
-      </Center>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
