ba128bc21f996b129e59e2f78efb3d52635d6e61
Colin <hiedra97@gmail.com>
2024-05-20 15:03:16 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    e.preventDefault();
-  }
-
-  function handleSubmitClick(e) {
-    e.preventDefault();
-  }
-
-  return (
-    <div>
-      <a
-        href="https://www.naver.com"
-        target={"_blank"}
-        onClick={handleLinkClick}
-      >
-        {" "}
-        네이버{" "}
-      </a>
-      <hr />
-      <form action="https://www.naver.com" onSubmit={handleSubmitClick}>
-        <input type="text" />
-        <button>전송</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
