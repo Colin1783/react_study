eb6be60a42912d37f98dba63a38e36a9486fae2f
Colin <hiedra97@gmail.com>
2024-05-20 10:36:18 +0900

    Flex, Center

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Flex, Spacer } from "@chakra-ui/react";
 
 function App(props) {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Flex justify={"space-between"}>
+        <Box bgColor={"blue"}>1</Box>
+        <Box bgColor={"orange"}>2</Box>
+        <Box bgColor={"yellow"}>3</Box>
+      </Flex>
+
+      <Flex>
+        <Spacer>
+          <Box bgColor={"green"}>1</Box>
+          <Box bgColor={"gold"}>2</Box>
+        </Spacer>
+        <Box bgColor={"green"}>3</Box>
+      </Flex>
+      <hr />
+      <Center>
+        <Box w={{ base: "100%", md: "5ppp" }} bgColor={"linen"}>
+          content
+        </Box>
+      </Center>
+    </ChakraProvider>
+  );
 }
 
 export default App;
