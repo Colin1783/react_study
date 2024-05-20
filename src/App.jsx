fe1955e99349bd5cb915d654f2620e3c86b75524
Colin <hiedra97@gmail.com>
2024-05-20 16:33:11 +0900

    // 배열을 map 메소드를 사용해 컴포넌트들을 만들 때,   // key prop을 사용해서 효율성을 높일 것

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  const name = ["January", "February", "March", "April", "May"];
+  //<li>January</li>..
+  // 배열을 map 메소드를 사용해 컴포넌트들을 만들 때,
+  // key prop을 사용해서 효율성을 높일 것
+  return (
+    <div>
+      <ul>
+        {name.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
