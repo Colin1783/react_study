6cf52d5c9b0b1085be4867c8a6c215e5448a1d8e
Colin <hiedra97@gmail.com>
2024-05-20 15:02:53 +0900

    기본동작 막기 연습 preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleClick(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <a href="https://www.naver.com" onClick={handleClick}>
+        구글
+      </a>
+      <hr />
+      <form action="https://www.google.com" onSubmit={handleClick}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
