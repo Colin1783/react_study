8c659d9978dc332cca479b89c3751a71400b8b11
Colin <hiedra97@gmail.com>
2024-05-20 14:33:52 +0900

    기본동작 막기 preventDefault

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  function handleLinkClick(e) {
+    e.preventDefault();
+  }
+
+  function handleSubmitClick(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <a
+        href="https://www.naver.com"
+        target={"_blank"}
+        onClick={handleLinkClick}
+      >
+        {" "}
+        네이버{" "}
+      </a>
+      <hr />
+      <form action="https://www.naver.com" onSubmit={handleSubmitClick}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
