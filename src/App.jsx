82cd87168dbffab1bcd6c13b964beab80d94defc
Colin <hiedra97@gmail.com>
2024-05-20 11:39:20 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
+import { ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
   const toast = useToast();
-  return (
-    <ChakraProvider>
-      <Button
-        onClick={() => {
-          toast({
-            title: "토스트 제목",
-            description: "토스트 내용",
-            position: "top-right",
-            status: "info",
-            isClosable: "top-right",
-          });
-        }}
-      ></Button>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
