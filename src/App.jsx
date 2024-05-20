c0b1fd001d0b5076f2c21da42b0d0d155373ff25
Colin <hiedra97@gmail.com>
2024-05-20 10:39:22 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
 import React from "react";
-import { Box, Center, ChakraProvider, Flex, Spacer } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Flex justify={"space-between"}>
-        <Box bgColor={"blue"}>1</Box>
-        <Box bgColor={"orange"}>2</Box>
-        <Box bgColor={"yellow"}>3</Box>
-      </Flex>
-
-      <Flex>
-        <Spacer>
-          <Box bgColor={"green"}>1</Box>
-          <Box bgColor={"gold"}>2</Box>
-        </Spacer>
-        <Box bgColor={"green"}>3</Box>
-      </Flex>
-      <hr />
-      <Center>
-        <Box w={{ base: "100%", md: "5ppp" }} bgColor={"linen"}>
-          content
-        </Box>
-      </Center>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
