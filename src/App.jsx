a18b1060548458d6407c19def9b8524cb22485fc
Colin <hiedra97@gmail.com>
2024-05-20 11:12:42 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,8 @@
 import React from "react";
-import {
-  ChakraProvider,
-  Checkbox,
-  Radio,
-  RadioGroup,
-  Switch,
-  Textarea,
-} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Checkbox>Lorem ipsum.</Checkbox>
-      <Checkbox>Necessitatibus, totam.</Checkbox>
-      <Checkbox>Consequuntur, doloremque!</Checkbox>
-      <Checkbox>Inventore, quas.</Checkbox>
-      <hr />
-      <RadioGroup>
-        <Radio value={1}>1</Radio>
-        <Radio value={2}>2</Radio>
-        <Radio value={3}>3</Radio>
-      </RadioGroup>
-      <hr />
-      <Switch />
-      <hr />
-      <Textarea>Lorem ipsum dolor sit amet, consectetur.</Textarea>
-    </ChakraProvider>
-  );
+  return <ChakraProvider></ChakraProvider>;
 }
 
 export default App;
