39d5abf3b60573c6ecdb8860193a90464b2e42a4
Colin <hiedra97@gmail.com>
2024-05-20 10:07:55 +0900

    m spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box m={1} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={2} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={10} bgColor={"tomato"}>
+        Cumque, omnis tempore.
+      </Box>
+      <Box m={20} bgColor={"tomato"}>
+        A, fugit, quas.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
